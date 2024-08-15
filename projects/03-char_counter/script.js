8b0b7beea6c67064957207b511dc83cd3bdcd373
Lubna Fathima <lubnafathima275@gmail.com>
2024-08-15 19:21:39 +0530

    feat: Added Char counter project

--- projects/03-char_counter/script.js
+++ projects/03-char_counter/script.js
@@ -2,4 +2,8 @@ let message = document.querySelector(".message");
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
 
-btn.addEventListener("click", () => message.innerText = "Hello " + input.value + "!");
+function update() {
+    message.textContent = input.value.length;
+}
+
+btn.addEventListener("click", update);
