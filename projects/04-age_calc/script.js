16506693b171d8c9d8ba955508c4c2a45b6a281b
Lubna Fathima <lubnafathima275@gmail.com>
2024-08-16 00:48:05 +0530

    feat: Age Calculator

--- projects/04-age_calc/script.js
+++ projects/04-age_calc/script.js
@@ -2,4 +2,13 @@ let message = document.querySelector(".message");
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
 
-btn.addEventListener("click", () => message.innerText = "Hello " + input.value + "!");
+btn.addEventListener("click", () => {
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let age = currentYear - input.value;
+    if(age >= 0) {
+        message.textContent = `You are ${age} years old`;
+    } else {
+        message.textContent = 'Enter a valid birth year!';
+    }
+});
