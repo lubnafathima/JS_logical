82dd6dc477192bfe67aa6b76cd07e2d0f01202da
Lubna Fathima <lubnafathima275@gmail.com>
2024-08-17 00:14:06 +0530

    feat: Tip Calculator

--- projects/05-tip_calc/script.js
+++ projects/05-tip_calc/script.js
@@ -1,5 +1,23 @@
 let message = document.querySelector(".message");
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
+let tip = document.querySelector(".tip");
 
-btn.addEventListener("click", () => message.innerText = "Hello " + input.value + "!");
+let selectedTip;
+tip.addEventListener("click", (e) => {
+    selectedTip = e.target.value;
+    message.innerText = "You selected " + selectedTip + "% tip";
+});
+
+btn.addEventListener("click", () => {
+    let bill = Number(input.value);
+    let tips = bill * selectedTip/100;
+    let totalAmout = bill + tips;
+    if(!selectedTip) {
+        message.innerText = "Whops, select a tip & try again!";
+    } else {
+        message.innerText = totalAmout;
+    }
+    input.value = "";
+    selectedTip = "";
+});
