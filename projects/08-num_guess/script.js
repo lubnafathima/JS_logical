df46bc1424ec15ab277389e6ec2388e3ee888029
Lubna Fathima <lubnafathima275@gmail.com>
2024-08-20 00:35:06 +0530

    feat: Number Gessing

--- projects/08-num_guess/script.js
+++ projects/08-num_guess/script.js
@@ -2,4 +2,22 @@ let message = document.querySelector(".message");
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
 
-btn.addEventListener("click", () => message.innerText = "Hello " + input.value + "!");
+let number = Math.floor(Math.random() * 10 + 1);
+
+btn.addEventListener("click", () => {
+  let guess = input.value;
+  
+  if (guess === "" || isNaN(guess)) {
+    message.textContent = "Enter a valid number";
+  } else {
+    if (guess > number) {
+      message.textContent = guess + " is too high!";
+    } else if (guess < number) {
+      message.textContent = guess + " is too low!";
+    } else {
+      message.textContent = guess + " is the correct number!";
+      number = Math.floor(Math.random() * 10);
+    }
+  }
+  input.value = "";
+});
